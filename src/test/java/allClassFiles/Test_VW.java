fd25d48797cd6daebbe0a614b00dd75c0155bd76
SaiPrasanth <50872218+SaiRamAdabala@users.noreply.github.com>
2019-09-23 23:24:05 +0530

    C19
    This is 19th commit c19

--- src/test/java/allClassFiles/Test_VW.java
+++ src/test/java/allClassFiles/Test_VW.java
@@ -7,7 +7,10 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+
 
 
 public class Test_VW {
@@ -52,6 +55,22 @@ driver.findElement(By.xpath("//input[@name='callback_2']")).click();
 w.until(ExpectedConditions.textToBePresentInElementLocated(By.xpath("//*[@id='wrapper']/header/section[2]/div/ul/li[1]/a"), "Dashboard"));
 /*//Wait Time
 Thread.sleep(40000);*/
+
+
+WebElement login=driver.findElement(By.xpath("//*[@id='wrapper']/header/section[2]/div/ul/li[1]/a"));
+String actualvp=login.getText();
+ //Verification Point1 for Login Page
+	if(actualvp.contains("Dashboard"))
+	{
+		System.out.println("Login Success,Test Passed");
+	}
+	else
+	{
+		System.out.println("Login Failed,Test Failed");
+	}
+	System.out.println(actualvp);
+	Thread.sleep(10000);
+
 }
 
 @Test(priority=3)
@@ -68,17 +87,16 @@ js.executeScript("window.scrollTo(0,document.body.scrollHeight);");
 Thread.sleep(5000);
 WebElement dashboard=driver.findElement(By.xpath("//h1[contains(text(),'Overall Performance')]"));
 String actualvp1=dashboard.getText();
-System.out.println(actualvp1);
- 
  //Verification Point1 for Dashboard Page
 	if(actualvp1.contains("Overall Performance"))
 	{
-		System.out.println("Login Success,Currently in Dashboard Page-Test Passed");
+		System.out.println("Currently in Dashboard Page-Test Passed");
 	}
 	else
 	{
-		System.out.println("Login Failed,Currently not in Dashboard Page-Test Failed");
+		System.out.println("Currently not in Dashboard Page-Test Failed");
 	}
+	System.out.println(actualvp1);
 	Thread.sleep(10000);
 }
 	
@@ -96,7 +114,7 @@ public void verifyTestsPage() throws InterruptedException
 	
 	WebElement tests= driver.findElement(By.xpath("//span[text()='Please select a test']"));
 	String actualvp2=tests.getText();
-	System.out.println(actualvp2);
+	
 	
 	
 	//Verification Point2 for Tests Page
@@ -108,6 +126,7 @@ public void verifyTestsPage() throws InterruptedException
 		{
 			System.out.println("Currently not in Tests Page-Test Failed");
 		}
+		System.out.println(actualvp2);
 		Thread.sleep(10000);
 }
 
@@ -128,7 +147,7 @@ Thread.sleep(25000);*/
 				
 		
 		String actualvp3=scripts.getText();
-		System.out.println(actualvp3);
+		
 		
 		
 		//Verification Point3 for Scripts Page
@@ -140,6 +159,7 @@ Thread.sleep(25000);*/
 			{
 				System.out.println("Currently not in Scripts Page-Test Failed");
 			}
+			System.out.println(actualvp3);
 			Thread.sleep(10000);
 }		
 			
@@ -164,7 +184,7 @@ public void logout_VoiceWatch() throws InterruptedException
 			
 			
 			String actualvp4=logout.getText();
-			System.out.println(actualvp4);
+			
 			if(actualvp4.contains("Sign in to continue"))
 			{
 				System.out.println("Logout Success-Test Passed");
@@ -173,6 +193,7 @@ public void logout_VoiceWatch() throws InterruptedException
 			{
 				System.out.println("Logout Failed-Test Failed");
 			}
+			System.out.println(actualvp4);
 			Thread.sleep(10000);
  //Close Browser
  
